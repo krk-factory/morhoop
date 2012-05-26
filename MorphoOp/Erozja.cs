84c4783dff0c40ff476dfb238b53849e2fa50dcb
Lukasz <kraluk@gmail.com>
2012-05-26 18:31:53 +0200

    Erodowania ciąg dalszy

--- MorphoOp/Erozja.cs
+++ MorphoOp/Erozja.cs
@@ -10,27 +10,41 @@ namespace MorphoOp
     {
         private Bitmap obrazWe;
         private Bitmap obrazWy;
+
         private ElementStrukt elS;
+
+        private int[,] strukturaElementu;
+
+        private int[,][,] tablicaErozyjna;
 
         
         public Erozja(Bitmap obraz, ElementStrukt elS)
         {
             this.obrazWe = obraz;
             this.elS = elS;
+            this.strukturaElementu = elS.StrukturaElementu;
 
             //wielkoscGranic = elS.WielkoscElementu;
         }
 
         public Bitmap zasadniczaErozja()
         {
-            inicjalizacjaObrazu(elS.WielkoscElementu);
+            //inicjalizacjaObrazu(elS.WielkoscElementu);
 
             return obrazWe;
         }
         
-        private void inicjalizacjaObrazu(int wielkoscElementu)
+        private void inicjalizacjaTablicy(int wielkoscElementu, int szerokoscObrazka, int wysokoscObrazka)
         {
-            
+            tablicaErozyjna = new int[wysokoscObrazka, szerokoscObrazka][,];
+
+            for (int k1 = 0; k1 < wysokoscObrazka; k1++)
+            {
+                for (int k2 = 0; k2 < szerokoscObrazka; k2++)
+                {
+                    tablicaErozyjna[k1, k2] = new int[wielkoscElementu, wielkoscElementu];
+                }
+            }
         }
 
 
